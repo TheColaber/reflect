08c1382ff239b22b53283ebf521002fe4ca77465
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-05-19 18:25:32 -0400

    ok attempt 3 then

--- js/service-worker.js
+++ js/service-worker.js
@@ -9,6 +9,6 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", function (event) {
-  // console.log('👷', 'fetch', event);
+  console.log("👷", "fetch", event);
   event.respondWith(fetch(event.request));
 });
